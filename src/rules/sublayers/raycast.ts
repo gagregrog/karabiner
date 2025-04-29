a2b014686db1af6022849e82d87d386763c14752
Gary Gregory Rogers <gagregrog@gmail.com>
2025-04-28 22:50:49 -0700

    feat: raycast sound

--- src/rules/sublayers/raycast.ts
+++ src/rules/sublayers/raycast.ts
@@ -6,10 +6,13 @@ import { LayerKeySublayer, open } from "../../utils";
 export const raycastLayer: LayerKeySublayer = {
   y: open("raycast://confetti"),
   c: open("raycast://extensions/thomas/color-picker/pick-color"),
-  n: open("raycast://script-commands/dismiss-notifications"),
   f: open("raycast://script-commands/copy-focused-finder-window-path"),
   j: open("raycast://script-commands/decode-jwt"),
+  m: open(
+    // first argument is for speakers, second is for mic
+    // double quoting is required for the second arument to pass through the shell_command
+    "'raycast://script-commands/audio-device-manager?arguments=LG&arguments=Yeti'"
+  ),
   a: open("raycast://script-commands/view-scripting-dictionary"),
-  m: open("raycast://script-commands/toggle-mic"),
   e: open("raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"),
 };
