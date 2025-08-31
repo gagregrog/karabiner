10918d0d2ada5c13ebd98c311ef1e8e0608e4913
Gary Gregory Rogers <gagregrog@gmail.com>
2025-08-31 10:59:44 -0700

    feat: mouseless everywhere

--- src/rules/fKeys.ts
+++ src/rules/fKeys.ts
@@ -1,16 +1,12 @@
-import { homeRowAppClick, homeRowAppScroll } from "./mappings";
 import { Rule } from "../types";
 import { remap, withConditions } from "../utils";
-import { isIntelCondition, isNotIntelCondition } from "../conditions";
+import { isNotIntelCondition } from "../conditions";
 
 export const fKeys: Rule = {
   description: "Make f keys do cool things",
   manipulators: [
-    // these fKey rules don't get triggered by other manipulators
-    withConditions(remap("f23", homeRowAppClick), isIntelCondition),
-    withConditions(remap("f24", homeRowAppScroll), isIntelCondition),
-    withConditions(remap("f23", { toKey: "f19" }), isNotIntelCondition), // used for Mouseless click
-    withConditions(remap("f24", { toKey: "f20" }), isNotIntelCondition), // used for Mouseless scroll
+    remap("f23", { toKey: "f19" }), // used for Mouseless click
+    remap("f24", { toKey: "f20" }), // used for Mouseless scroll
     withConditions(
       remap("f22", { toKey: "f18", toModifiers: ["left_command"] }), // used to trigger Ice search
       isNotIntelCondition // **Ice not supported on intel**
