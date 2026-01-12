3a0ed149f9e76c0734c93703b47275faed93fd6d
Gary Gregory Rogers <gagregrog@gmail.com>
2026-01-12 10:41:44 -0800

    --ammend

--- src/rules/spaceLayers.ts
+++ src/rules/spaceLayers.ts
@@ -1,13 +1,13 @@
 import { KeyCode } from "../types";
-import { to, duplicateLayer, makeLayers, trackedKeyInactive } from "../utils";
+import { duplicateLayer, makeLayers, trackedKeyInactive } from "../utils";
 import { applicationsLayer } from "./sublayers/applications";
 import { browseLayer } from "./sublayers/browse";
 import { deskLayer } from "./sublayers/desk";
+import { homerowMovementKeys } from "./sublayers/homerowMovement";
 import { raycastLayer } from "./sublayers/raycast";
 import { systemLayer } from "./sublayers/system";
 import { windexLayer } from "./sublayers/windex";
 import { yabaiLayer } from "./sublayers/yabai";
-import { yabaiMovementLayer } from "./sublayers/yabaiMovement";
 import { LAYERS_NAME, SHIFT_MOD } from "./variableNames";
 
 const activators: KeyCode[] = ["spacebar", "delete_or_backspace"];
@@ -16,27 +16,23 @@ export const spaceLayers = makeLayers(
   LAYERS_NAME,
   activators,
   {
-    // movement keys for right hand
-    m: to("left_arrow"),
-    n: to("down_arrow"),
-    e: to("up_arrow"),
-    i: to("right_arrow"),
-    t: to("tab", ["command"]),
-
+    // mnei to arrows, caps modified to command arrows, t for cmd+tab
+    ...homerowMovementKeys,
     d: deskLayer,
     b: browseLayer,
     ...duplicateLayer(["a", "o"], applicationsLayer),
     w: windexLayer,
-    ...duplicateLayer(
-      [
-        // return_or_enter/caps_lock useful on builtin keyboard
-        "return_or_enter",
-        "caps_lock",
-        // activators useful on external keyboard for mirrored layout
-        ...activators,
-      ],
-      yabaiMovementLayer
-    ),
+    // ...duplicateLayer(
+    //   [
+    //     // return_or_enter/caps_lock useful on builtin keyboard
+    //     "return_or_enter",
+    //     "caps_lock",
+    //     // activators useful on external keyboard for mirrored layout
+    //     ...activators,
+    //   ],
+    //   yabaiMovementLayer
+    // ),
+
     y: yabaiLayer,
     s: systemLayer,
     r: raycastLayer,
---
+++ src/rules/sublayers/homerowMovement.ts
@@ -0,0 +1,32 @@
+import { KeyCode } from "../../types";
+import { LayerCommand, Sublayers, to } from "../../utils";
+
+const arrowMappings = [
+  ["m", "left_arrow"],
+  ["n", "down_arrow"],
+  ["e", "up_arrow"],
+  ["i", "right_arrow"],
+] as const satisfies Array<[KeyCode, KeyCode]>;
+
+type ArrowMapping = Record<(typeof arrowMappings)[number][0], LayerCommand>;
+
+/**
+ * Home Row arrow navigation + capslock -> command modified
+ */
+export const homerowMovementKeys = {
+  ...arrowMappings.reduce(
+    (acc, [fromCode, toCode]) => ({
+      ...acc,
+      [fromCode]: to(toCode),
+    }),
+    {} as ArrowMapping
+  ),
+  caps_lock: arrowMappings.reduce(
+    (acc, [fromCode, toCode]) => ({
+      ...acc,
+      [fromCode]: to(toCode, ["command"]),
+    }),
+    {} as ArrowMapping
+  ),
+  t: to("tab", ["command"]),
+} satisfies Sublayers;
