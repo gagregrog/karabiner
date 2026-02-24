4b658715e02182cd28b08a9ced573396f14a6645
Gary Gregory Rogers <gagregrog@gmail.com>
2026-02-23 16:13:32 -0800

    fix: phone status

--- src/rules/sublayers/phone.ts
+++ src/rules/sublayers/phone.ts
@@ -9,7 +9,7 @@ export const phoneLayer: LayerKeySublayer = {
   return_or_enter: phonePost("ring/us", "Ring the phone"),
   delete_or_backspace: phonePost("ring/stop", "Stop the phone ringing"),
   5: phonePostWithDisplay("timer/5m", "Set a 5 minute timer"),
-  s: phoneGet("ring/status", "Show ring status"),
+  s: phoneGet("status", "Show ring status"),
   l: phonePost("ring/chirp/1", "Chirp the phone"),
   c: phonePostWithDisplay("timer/cancel", "Cancel running timer"),
   t: phoneTimerPrompt(),
