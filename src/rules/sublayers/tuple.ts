c6ca354354fcd3d893aa7495171de075d38228fa
Gary Gregory Rogers <gagregrog@gmail.com>
2026-04-06 10:16:02 -0700

    feat: tuple

--- src/rules/sublayers/tuple.ts
+++ src/rules/sublayers/tuple.ts
@@ -1,20 +1,19 @@
-import { LayerKeySublayer, to, withConditions } from "../../utils";
+import { KeyCode } from "../../types";
+import { LayerCommand, LayerKeySublayer, to, withConditions } from "../../utils";
 import { isNotIntelCondition } from "../../conditions";
 
-/**
- * Tuple shortcuts
- */
-export const tupleLayer: LayerKeySublayer = {
-  m: withConditions(
-    to("backslash", ["option", "shift", "command"]),
-    isNotIntelCondition
-  ),
-  e: withConditions(
-    to("quote", ["option", "shift", "command"]),
-    isNotIntelCondition
-  ),
-  delete_or_backspace: withConditions(
-    to("grave_accent_and_tilde", ["option", "shift", "command"]),
-    isNotIntelCondition
-  ),
+const tupleCommands: Partial<Record<KeyCode, LayerCommand>> = {
+  m: to("backslash", ["option", "shift", "command"]),
+  e: to("quote", ["option", "shift", "command"]),
+  delete_or_backspace: to("grave_accent_and_tilde", ["option", "shift", "command"]),
+  h: to("1", ["option", "shift", "command"]),
+  comma: to("2", ["option", "shift", "command"]),
+  period: to("3", ["option", "shift", "command"]),
 };
+
+export const tupleLayer: LayerKeySublayer = Object.fromEntries(
+  Object.entries(tupleCommands).map(([key, command]) => [
+    key,
+    withConditions(command as LayerCommand, isNotIntelCondition),
+  ])
+) as LayerKeySublayer;
